7500510fdd4f721a0eddf40f406ada6f325b828d
isai gamboa <19211962@utcentro.onmicrosoft.com>
2022-06-23 23:19:02 -0500

    se agrego contenido en la carpeta componentes

---
+++ resources/js/components/AuthorsContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container rounded shadow">
+    <div class="row align-items-center justify-content-between p-2">
+      <div class="col-6 fs-3">Autores</div>
+      <div class="col-6 d-flex justify-content-end">
+        <button class="btn btn-primary">Agregar</button>
+      </div>
+    </div>
+    <slot :data="{ message: 'hola' }" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- resources/js/components/TableComponent.vue
+++ resources/js/components/TableComponent.vue
@@ -1,9 +1,28 @@
 <template>
-  <span>This is Table component</span>
+  <table class="table">
+    <thead>
+      <tr class="text-center">
+        <th scope="col">#</th>
+        <th v-for="(head, i) in headings" :key="i">{{ head }}</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="text-center" v-for="(row, i) in contents" :key="i">
+        <td v-for="prop of row" :key="prop">
+          {{ prop ? prop : 'No definido' }}
+        </td>
+        <td class="d-flex justify-content-center align-items-center gap-2">
+          <button class="btn btn-warning">Editar</button>
+          <button class="btn btn-danger">Eliminar</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
-
+  props: ['headings', 'contents'] // -> ['id', 'name']
 }
 </script>
