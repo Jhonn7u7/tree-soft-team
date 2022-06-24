87b3d2d946781c16252510b71623c702ba6fdbff
-e <erikciau777@icloud.com>
2022-06-24 11:37:04 -0500

    se agrego el formulario para editar y un methodo para obtener parametros desde la url

--- resources/js/components/AuthorsContainer.vue
+++ resources/js/components/AuthorsContainer.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="container rounded shadow">
-    <modal-component title="Agregar autor">
+    <modal-component title="Agregar autor" trigger="agregar">
       <template #body>
         <form @submit.prevent="agregarNuevoAutor">
+          <div class="mb-3">
+            <label for="username" class="form-label">Nombre de autor</label>
+            <input v-model="formulario.username" type="text" class="form-control" id="username">
+            <div id="username" class="form-text text-danger">El campo no puede estar vacio</div>
+          </div>
+          <div class="mb-3">
+            <label for="lastname" class="form-label">Apellido de autor</label>
+            <input v-model="formulario.lastname" type="text" class="form-control" id="lastname">
+            <div id="lastname" class="form-text text-danger">El campo no puede estar vacio</div>
+          </div>
+          <div class="mb-3">
+            <label for="location" class="form-label">Locación de autor</label>
+            <input v-model="formulario.location" type="text" class="form-control" id="location">
+          </div>
+          <button type="submit" class="btn btn-primary">Subir</button>
+        </form>
+      </template>
+    </modal-component>
+    <modal-component title="Editar autor" trigger="editar">
+      <template #body>
+        <form @submit.prevent="editarAutor">
           <div class="mb-3">
             <label for="username" class="form-label">Nombre de autor</label>
             <input v-model="formulario.username" type="text" class="form-control" id="username">
@@ -24,7 +45,7 @@
     <div class="row align-items-center justify-content-between p-2">
       <div class="col-6 fs-3">Autores</div>
       <div class="col-6 d-flex justify-content-end">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregar">
           Agregar
         </button>
       </div>
@@ -81,6 +102,12 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    obtenerParametrosDeUrl(parametroABuscar) {
+      const parametro = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
+      return parametro[parametroABuscar]
     }
   },
   //fin de methods}
